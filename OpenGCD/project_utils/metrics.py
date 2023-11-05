3cb61c18d536c7d5e834f6355795f9c026d6592e
FulinGao <49235982+Fulin-Gao@users.noreply.github.com>
2023-11-05 16:28:56 +0800

    Add files via upload

--- OpenGCD/project_utils/metrics.py
+++ OpenGCD/project_utils/metrics.py
@@ -63,7 +63,7 @@ def evaluation_closed(y_true, y_pred, phase, args):
     :param y_pred:  predicted label n*1
     :param phase:   current stage
     :param args:    various parameters
-    :print:         Accuracy
+    :print:         Accuracy, F1-Score, Confusion Matrix
     """
     print("Performing {} closed-set recognition evaluation".format(phase))
     stage_name = ['1st', '2nd', '3rd', '4th']
@@ -75,10 +75,22 @@ def evaluation_closed(y_true, y_pred, phase, args):
             condition = (args.class_splits[p-1] <= y_true) & (y_true < args.class_splits[p])
 
         acc = accuracy_score(y_true[condition], y_pred[condition])
+        f1 = f1_score(y_true[condition], y_pred[condition], average="weighted")
+        cm = confusion_matrix(y_true[condition], y_pred[condition])
+
         print("Accuracy on {} test set: {:.3f}".format(stage_name[p], acc))
+        print("F1-Score on {} test set: {:.3f}".format(stage_name[p], f1))
+        # print("Confusion Matrix on {} test set: ".format(stage_name[p]))
+        # print(cm)
 
     acc = accuracy_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred, average="weighted")
+    cm = confusion_matrix(y_true, y_pred)
+
     print("Accuracy on all test set: {:.3f}".format(acc))
+    print("F1-Score on all test set: {:.3f}".format(f1))
+    # print("Confusion Matrix on all test set: ")
+    # print(cm)
 
 
 def evaluation_open(y_true, y_pred, phase):
@@ -86,12 +98,19 @@ def evaluation_open(y_true, y_pred, phase):
     :param y_true:     ground-true open-set label   n*1
     :param y_pred:     predicted open-set label     n*1
     :param phase:      current stage
-    :print:            HHA
+    :print:            HHA, OSFM, Confusion Matrix
     """
     print("Performing {} open-set recognition evaluation".format(phase))
+    cm = confusion_matrix(y_true, y_pred)
     HNA = metrics_HNA(y_pred, y_true, np.max(y_true))
+    OSFM = metrics_OSFM(cm)
+
     print("HNA: {:.3f}".format(HNA))
-    return HNA, None
+    print("weighted OSFM: {:.3f}".format(OSFM))
+    print("Confusion Matrix: ")
+    print(cm)
+
+    return HNA, OSFM
 
 
 def evaluation_novel(y_true, y_pred, phase, args):
@@ -104,13 +123,31 @@ def evaluation_novel(y_true, y_pred, phase, args):
     :print:              clustering accuracy on old, new, all classes
     """
 
+    # previous metric
+    # ind_new = ind[ind[:,-1] >= num_class_seen]
+    # acc_new = sum([w[i, j] for i, j in ind_new]) * 1.0 / y_pred[y_ture >= num_class_seen].size
+    #
+    # stage_name = ['1st', '2nd', '3rd', '4th']
+    # for p in range(int(phase[0])):
+    #     if p == 0:
+    #         condition = y_ture < args.class_splits[p]
+    #         ind_old = ind[ind[:, -1] < args.class_splits[p]]
+    #     else:
+    #         condition = (args.class_splits[p-1] <= y_ture) & (y_ture < args.class_splits[p])
+    #         ind_old = ind[(args.class_splits[p-1] <= ind[:, -1]) & (ind[:, -1] < args.class_splits[p])]
+    #
+    #     acc_old = sum([w[i, j] for i, j in ind_old]) * 1.0 / y_pred[condition].size
+    #     print("Clustering accuracy on {} test set from seen classes: {:.3f}".format(stage_name[p], acc_old))
+    #
+    # print("Clustering accuracy on new classes: {:.3f}".format(acc_new))
+
     print("Performing {} generalized novel category discovery evaluation".format(phase))
     num_class_seen = args.class_splits[int(phase[0])-1]
 
     # Accuracy of seen classes
     condition_seen = y_true < num_class_seen
     acc_seen = accuracy_score(y_true[condition_seen], y_pred[condition_seen])
-
+    print("AKS: {:.3f}".format(acc_seen))
     condition_novel = (y_true >= num_class_seen) & (y_pred >= num_class_seen)
     if acc_seen == 0 or np.all(condition_novel == False):
         HCA = 0
@@ -121,5 +158,9 @@ def evaluation_novel(y_true, y_pred, phase, args):
         acc_novel = sum([w[i, j] for i, j in ind_novel]) * 1.0 / y_true[y_true >= num_class_seen].size
         # Harmonic clustering accuracy
         HCA = 2 / (1 / acc_seen + 1 / acc_novel)
+        print("ANS: {:.3f}".format(acc_novel))
 
     print("Harmonic clustering accuracy: {:.3f}".format(HCA))
+    print("Confusion Matrix: ")
+    cm = confusion_matrix(y_true, y_pred)
+    print(cm)
