f4d2f01c090809d5f50c51aa5fda92528ff83fee
FulinGao <49235982+Fulin-Gao@users.noreply.github.com>
2024-03-22 07:00:36 +0800

    Update open_world_imagenet.py

--- OpenGCD/data/open_world_imagenet.py
+++ OpenGCD/data/open_world_imagenet.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 
 def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
